e4a8624cf1b6aab92a96ee8f4a0c7b642d11c672
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2021-04-13 16:23:44 -0400

    react-native example code

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,16 +1,40 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 const BoxScreen = () => {
-    return (
-        <View>
-            <Text>Fart box</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  parentStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red",
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "purple",
+    alignSelf: "flex-end"
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: "green",
+  },
+});
 
-})
-
-export default BoxScreen
+export default BoxScreen;
